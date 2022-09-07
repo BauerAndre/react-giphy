e28352743b0992666e1ed25c6d69c92b57ad5a59
Andre Bauer <andrebauer@live.com>
2022-09-07 20:15:09 +0900

    gift site

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,27 @@
 import '../assets/stylesheets/application.scss';
+
+import React from 'react';
+import ReactDom from 'react-dom/client';
+
+// import App from "./components/app"
+// const Hello = (props) => {
+//   const { name, job } = props;
+//   return <h1> Hello, I am { name } and I am a { job }!!! </h1>;
+// };
+
+// class Hello extends React.Component{
+//   render(){
+//     const { name, job } = props;
+//     return (
+//     <h1>
+//       Hello, I am { name } and I am a { job }!!!
+//     </h1>
+//     )
+//   }
+// }
+
+// const rootContainer = document.querySelector('#root');
+// if (rootContainer) {
+//   const root = ReactDom.createRoot(rootContainer);
+//   root.render(<Hello name="Andre" job="Dev" />);
+// }
