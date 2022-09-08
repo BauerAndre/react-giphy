27bbd7f424385a2fb80f11692afc151ff9e7f2c1
Andre Bauer <andrebauer@live.com>
2022-09-08 09:45:23 +0900

    gift search with react

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,27 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 import '../assets/stylesheets/application.scss';
 
-import React from 'react';
-import ReactDom from 'react-dom/client';
+import App from './components/App';
 
-// import App from "./components/app"
-// const Hello = (props) => {
-//   const { name, job } = props;
-//   return <h1> Hello, I am { name } and I am a { job }!!! </h1>;
-// };
-
-// class Hello extends React.Component{
-//   render(){
-//     const { name, job } = props;
-//     return (
-//     <h1>
-//       Hello, I am { name } and I am a { job }!!!
-//     </h1>
-//     )
-//   }
-// }
-
-// const rootContainer = document.querySelector('#root');
-// if (rootContainer) {
-//   const root = ReactDom.createRoot(rootContainer);
-//   root.render(<Hello name="Andre" job="Dev" />);
-// }
+const container = document.getElementById('root');
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  root.render(<App />);
+}
